b6801b6474b39a0d64d64fafe178b10b489ec11f
Milagros-Blasco <milagrosblascogaona@gmail.com>
2024-02-24 22:31:02 -0300

    footer2

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="left-content">
+        <img src="images/footer/nota_dorada.png" alt="Isologotipo de la empresa" className="logo_blanco" />
+        <img src="images/footer/logo_blanco.png" alt="Isologotipo de la empresa" className="logo_blanco" />
+        <span className="copyright">&copy; {year}</span>
+      </div>
+      <div className='right-content'>
+        <img src="images/footer/instagram.png" alt="Instagram" />
+        <img src="images/footer/linkedin.png" alt="Linkedin" />
+        <img src="images/footer/tiktok.png" alt="TikTok" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
