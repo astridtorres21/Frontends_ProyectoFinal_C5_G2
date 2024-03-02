d889d885879c4eb2b4757304df802112d5667e67
Milagros-Blasco <milagrosblascogaona@gmail.com>
2024-03-02 11:10:40 -0300

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import {
     , Route 
     , Routes
 } from 'react-router-dom';
+import Login from './components/Login';
 import Register from './components/Register';
 import Search from './components/Search';
 import ProductDetail from './components/ProductDetail';
