b536881fac4df11a225f9f6e3e3532e89d9d625c
katerineavila <katerineavila@hotmail.com>
2024-03-02 09:07:21 -0500

    Se realizan ajustes al archivo App.jsx y se agrega la funcionalidad inicial de ProductDetail

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import './css/Footer.css';
 
-
-
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <footer className="footer">
       <div className="left-content">
-        <img src="images/footer/nota_dorada.png" alt="Isologotipo de la empresa" className="logo_nota" />
-        <img src="images/footer/logo_blanco.png" alt="Isologotipo de la empresa" className="logo_blanco" />
+        <img src={process.env.PUBLIC_URL + "/images/footer/nota_dorada.png"} alt="Isologotipo de la empresa" className="logo_nota" />
+        <img src={process.env.PUBLIC_URL + "/images/footer/logo_blanco.png"} alt="Isologotipo de la empresa" className="logo_blanco" />
         <span className="copyright">&copy; {year}</span>
       </div>
       <div className='right-content'>
-        <img src="images/footer/instagram.png" alt="Instagram" />
-        <img src="images/footer/linkedin.png" alt="Linkedin" />
-        <img src="images/footer/tiktok.png" alt="TikTok" />
+        <img src={process.env.PUBLIC_URL + "/images/footer/instagram.png"} alt="Instagram" />
+        <img src={process.env.PUBLIC_URL + "/images/footer/linkedin.png"} alt="Linkedin" />
+        <img src={process.env.PUBLIC_URL + "/images/footer/tiktok.png"} alt="TikTok" />
       </div>
     </footer>
   );
--- frontend/src/components/ProductDetail.jsx
+++ frontend/src/components/ProductDetail.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/ProductDetail.css';
 
 const ProductDetail = ({ match }) => {
-  //const productId = match.params.id;
-
-  // Lógica para obtener detalles del producto con ID 'productId'
-
   return (
     <div>
-      <h2>Detalles del Producto</h2>
-      {/* Mostrar detalles del producto */}
+      {/* Primer bloque */}
+      <div className="productDetail-header">
+        <div className="productDetail-header-title-left">
+          <h1>Titulo del Producto 1</h1>
+        </div>
+        <div className="productDetail-header-right">
+          <Link to="/">
+            <button>Volver Atrás</button>
+          </Link>
+        </div>
+      </div>
+
+      {/* Segundo bloque */}
+      <div className="productDetail-header">
+        <div className="productDetail-header-title-left">
+          <div className="col-6 col-s-9 product-detail-main-image">
+            <img src="https://www.eltiempo.com/files/article_main_1200/uploads/2023/03/09/640a5f6c7f159.jpeg" alt="Main" />
+          </div>
+
+          <div className="col-3 col-s-12">
+            <div className="aside">
+              <div className="product-detail-row">
+                <img src="https://www.eltiempo.com/files/article_main_1200/uploads/2023/03/09/640a5f6c7f159.jpeg" alt="Image1" />
+              </div>
+              <div className="product-detail-row">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_2X_996993-MLU69825293985_062023-F.webp" alt="Image2" />
+              </div>
+              <div className="product-detail-row">
+                <img src="https://www.eltiempo.com/files/article_main_1200/uploads/2023/03/09/640a5f6c7f159.jpeg" alt="Image1" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="productDetail-header-right  ">
+          <h5>Descripción del Producto</h5>
+        </div>
+      </div>
     </div>
   );
 }
