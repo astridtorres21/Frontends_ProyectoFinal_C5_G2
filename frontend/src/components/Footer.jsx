c997e435ed3206074307745bbb974d7f4a43b10e
Milagros-Blasco <milagrosblascogaona@gmail.com>
2024-03-02 15:31:16 -0300

    footerdesktop

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,18 +11,21 @@ const Footer = () => {
       <div className="left-content">
         <img src="images/footer/nota_dorada.png" alt="Isologotipo de la empresa" className="logo_nota" />
         <img src="images/footer/logo_blanco.png" alt="Isologotipo de la empresa" className="logo_blanco" />
-        <div>
+        <div className='copy'>
         <span className="copyright">&copy; Doremi {year}. Todos los derechos reservados</span>
         </div>
       </div>
       <div className='right-content'>
-        <div>
+        <div className='social-medias-title'>
           <h4>Seguinos en nuestras redes sociales</h4>
         </div>
+        <div className='social-medias'>
+        <img src="images/footer/facebook.png" alt="Facebook" />
         <img src="images/footer/instagram.png" alt="Instagram" />
-        <img src="images/footer/linkedin.png" alt="Linkedin" />
-        <img src="images/footer/tiktok.png" alt="TikTok" />
         <img src="images/footer/X.png" alt="X" />
+        <img src="images/footer/tiktok.png" alt="TikTok" />
+        </div>
+
         </div>
     </footer>
   );
