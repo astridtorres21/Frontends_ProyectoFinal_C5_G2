5657fe84aeddb35e36f07a29161bc12fb7b7bcdd
Milagros-Blasco <milagrosblascogaona@gmail.com>
2024-03-20 19:26:00 -0300

    newheader

--- frontend/src/components/NewHeader.jsx
+++ frontend/src/components/NewHeader.jsx
@@ -1,27 +1,39 @@
-import React, { useState } from 'react';
-import './css/NewHeader.css';
+import React from 'react';
+import Avatar from 'react-avatar'; // Importa el componente Avatar de react-avatar
+
 function NewHeader() {
-  const [username, setUsername] = useState('Usuario');
-  const handleUsernameChange = () => {
-    const newName = prompt('Por favor, ingresa tu nombre de usuario:');
-    if (newName) {
-      setUsername(newName);
+  // Función para obtener el nombre de usuario desde el almacenamiento local
+  const getUserName = () => {
+    return localStorage.getItem('userName') || ''; // Si el nombre de usuario no está definido, devuelve una cadena vacía
+  };
+
+  // Función para manejar el cierre de sesión
+  const handleLogout = () => {
+    const confirmLogout = window.confirm('¿Estás seguro que quieres cerrar sesión?');
+    if (confirmLogout) {
+      localStorage.removeItem('userName'); // Elimina el nombre de usuario del almacenamiento local al cerrar sesión
+      window.location.href = '/logout'; // Redirige al usuario a otra página después de cerrar sesión
     }
   };
 
   return (
-    <div className="newheader">
+    <header>
       <div className="logo">
-        <img src="ruta/al/logo.png" alt="Logo" />
+        <img src="images/header.png" alt="logo" />
       </div>
       <div className="user-info">
-        <p>Bienvenido, {username}</p>
-        <img src="ruta/al/foto-usuario.jpg" alt="Foto de Usuario" />
+        {/* Mostramos el nombre del usuario */}
+        <span>{getUserName()}</span>
+        {/* Mostramos el avatar del usuario utilizando el componente Avatar de react-avatar */}
+        <Avatar name={getUserName()} size="40" round={true} />
+        {/* Botón de cerrar sesión con confirmación */}
+        <button onClick={handleLogout}>Cerrar sesión</button>
       </div>
-      <button onClick={handleUsernameChange}>Cambiar Usuario</button>
-    </div>
+    </header>
   );
 }
 
 export default NewHeader;
+
+
 
