b93943b1da30f591f99ac7f8def69a0918ca163a
capireque <carolina2001prq@gmail.com>
2024-03-20 21:05:01 -0500

    Revert "cambios3"
    This reverts commit 35e4e054d1a5dee1fb11ef9abbf33dccd9da9521, reversing
    changes made to baf65b49ccbdfe6a38c7ff62799228eecb683422.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { 
+    BrowserRouter as Router
+    , Route 
+    , Routes
+} from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Search from './components/Search';
@@ -10,11 +14,10 @@ import Admin from './components/Admin';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-import NewHeader from './components/NewHeader';
+
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>      
         <Routes>
         <Route path="/" element={<Home/>} />
@@ -25,25 +28,6 @@ function App() {
         <Route path="/admin" element={<Admin/>} />
       </Routes>
     </Router>
-=======
-    <div>
-      <NewHeader />
-      <Search />
-      <Body/>
-      <Footer />
-      
-      <Router>      
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/product/:id" element={<ProductDetail/>} />
-          <Route path="/confirm-reservation" element={<ReservationConfirmation/>} />
-          <Route path="/admin" element={<Admin/>} />
-        </Routes>
-      </Router>
-    </div>
->>>>>>> feature_fe_newhome
   );
 }
 
